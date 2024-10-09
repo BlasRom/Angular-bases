6afb75e5ae8446f3b337771140f1891c23fd6c29
Blasrom <daniel.romero@dstx.mx>
2024-10-09 16:37:09 -0600

    Commit 09-10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
-
+ const a: number=1;
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
